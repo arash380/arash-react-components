357835838a74d51dc535dcfa9f42689c39f4c7cb
asa510 <arash_saadati@sfu.ca>
2024-12-27 09:42:30 -0800

    app loader bug fix

--- src/Components/AppLoader/AppLoader.js
+++ src/Components/AppLoader/AppLoader.js
@@ -4,9 +4,9 @@ import { DEFAULT_THEME } from "../ThemeProvider/ThemeProvider";
 import classes from "./AppLoader.module.css";
 
 // TODO: get a different type of the loader from outside
-const AppLoader = () => (
+const AppLoader = ({ color = DEFAULT_THEME.colors.primary }) => (
   <div className={classes.root}>
-    <PropagateLoader color={DEFAULT_THEME.colors.primary} />
+    <PropagateLoader color={color} />
   </div>
 );
 
